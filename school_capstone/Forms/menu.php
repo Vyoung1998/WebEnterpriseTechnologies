c567a015cbf46fd402a57a598c85f994437c9b5f
Victoria Young <50598538+Vyoung1998@users.noreply.github.com>
2026-06-07 20:56:22 -0400

    Delete school_capstone directory

--- school_capstone/Forms/menu.php
+++
@@ -1,8 +0,0 @@
-<nav>
-    <ul>
-        <li><h1>Feedback Forms</h1></li>
-        <li><a href="home.php">Home</a></li>
-    </ul>
-</nav>
-
-<!--this was found from the book provided in class already sited on main pages.-->
