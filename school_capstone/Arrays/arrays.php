43d64b770fed3627e16865ab9f4bf89a5a446391
Victoria Young <50598538+Vyoung1998@users.noreply.github.com>
2026-06-07 20:43:36 -0400

    Week 3 upload, includes array assignment

---
+++ school_capstone/Arrays/arrays.php
@@ -0,0 +1,65 @@
+<?php
+   return $employees = [
+            //----Digital Department
+            //Web Development and IT Team
+            //emp1
+            ['FirstName' => 'Victoria', 'LastName' => 'Young', 'Department' => 'Digital Department', 'Team' => 'Web Development and IT Team', 
+            'Email' => 'vyoung123@email.com', 'Phone' => '111-111-1111', 'Age' => '27', 
+            'Role'  => 'Software Engineer', 'Favorite Scripture' => 'Romans 8:37'],
+            //emp2
+            ['FirstName' => 'Brandon', 'LastName' => 'Utah', 'Department' => 'Digital Department', 'Team' => 'Web Development and IT Team', 
+            'Email' => 'butah123@email.com', 'Phone' => '222-222-2222', 'Age' => '37', 
+            'Role'  => 'IT Admin', 'Favorite Scripture' => 'Psalm 23: 1-3'],
+
+            //Marketing Team
+            //emp3
+             ['FirstName' => 'Cheryl', 'LastName' => 'Betts', 'Department' => 'Digital Department', 'Team' => 'Marketing Team', 
+            'Email' => 'cbetts123@email.com', 'Phone' => '333-333-3333', 'Age' => '48', 
+            'Role'  => 'Vendor/Sales', 'Favorite Scripture' => 'Jeremiah 29:11'],
+            //emp4
+            ['FirstName' => 'Emily', 'LastName' => 'Pennyworth', 'Department' => 'Digital Department', 'Team' => 'Marketing Team', 
+            'Email' => 'epennyworth123@email.com', 'Phone' => '444-444-4444', 'Age' => '27', 
+            'Role' => 'Blogger', 'Favorite Scripture' => '2 Timothy 1:6'],
+
+            //----Cafe and Coffee Department
+            //Coffee Team
+            //emp 5
+            ['FirstName' => 'Jason', 'LastName' => 'Hallow', 'Department' => 'Cafe and Coffee', 'Team' => 'Coffee Team', 
+            'Email' => 'jhallow123@email.com', 'Phone' => '555-555-555', 'Age' => '23', 
+            'Role' => 'Barista', 'Favorite Scripture' => 'Romans 8:37'],
+            //emp 6
+            ['FirstName' => 'Deonte', 'LastName' => 'Black', 'Department' => 'Cafe and Coffee', 'Team' => 'Coffee Team', 
+            'Email' => 'dblack123@email.com', 'Phone' => '666-666-6666', 'Age' => '25', 
+            'Role' => 'Barista', 'Favorite Scripture' => 'Joshua 1:9'],
+
+            //Kitchen Team
+            //emp 7
+            ['FirstName' => 'Oscar', 'LastName' => 'Whitworth', 'Department' => 'Cafe and Coffee', 'Team' => 'Kitchen Team', 
+            'Email' => 'owhitworth123@email.com', 'Phone' => '777-777-7777', 'Age' => '45', 
+            'Role' => 'Baker', 'Favorite Scripture' => 'John 3:16'],
+            //emp 8
+            ['FirstName' => 'Timothy', 'LastName' => 'Jackson', 'Department' => 'Cafe and Coffee', 'Team' => 'Kitchen Team', 
+            'Email' => 'tblack123@email.com', 'Phone' => '888-888-8888', 'Age' => '38', 
+            'Role' => 'Baker', 'Favorite Scripture' => 'Philippians 4:13'],
+
+            //----Operations Department
+            //Receiving Team
+            //emp 9
+            ['FirstName' => 'Lucy', 'LastName' => 'Yolanda', 'Department' => 'Operations Department', 'Team' => 'Receiving Team', 
+            'Email' => 'lyolanda123@email.com', 'Phone' => '999-999-9999', 'Age' => '29', 
+            'Role' => 'Inventory Manager', 'Favorite Scripture' => 'John 14:21'],
+            //emp 10
+            ['FirstName' => 'Alex', 'LastName' => 'Smith', 'Department' => 'Operations Department', 'Team' => 'Receiving Team', 
+            'Email' => 'asmith123@email.com', 'Phone' => '101-101-1010', 'Age' => '32', 
+            'Role' => 'Inventory Logger', 'Favorite Scripture' => 'John 4: 18'],
+
+            //Shipping Team
+            //emp 11
+             ['FirstName' => 'Susan', 'LastName' => 'Redford', 'Department' => 'Operations Department', 'Team' => 'Shipping Team', 
+            'Email' => 'sredford123@email.com', 'Phone' => '110-110-1100', 'Age' => '39', 
+            'Role' => 'Packing Associate', 'Favorite Scripture' => 'Romans 6: 23'],
+            //emp 12
+             ['FirstName' => 'Andrew', 'LastName' => 'Scott', 'Department' => 'Operations Department', 'Team' => 'Shipping Team', 
+            'Email' => 'ascott123@email.com', 'Phone' => '120-120-1200', 'Age' => '42', 
+            'Role' => 'Shipping Manager', 'Favorite Scripture' => 'Proverbs 3: 13-14']
+        ];
