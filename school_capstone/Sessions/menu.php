cd9baad16198607db07d5485c0476e48dabf5fe7
Victoria Young <50598538+Vyoung1998@users.noreply.github.com>
2026-06-14 23:41:24 -0400

    Delete school_capstone directory

--- school_capstone/Sessions/menu.php
+++
@@ -1,6 +0,0 @@
-<nav>
-    <ul>
-        <li><a href="login.php">Login</a></li>
-        <li><a href="home_loggedin.php">Home</a></li>
-    </ul>
-</nav>
--- school_capstone/Sessions/menu2.php
+++
@@ -1,9 +0,0 @@
-<nav>
-    <ul>
-        <li><a href="login.php">Login</a></li>
-        <li><a href="home_loggedin.php">Home</a></li>
-        <li><a href="profile.php">Your Profile</a></li>
-    </ul>
-</nav>
-
-<!--added this secondary menu, profile only shows when logged in.-->
