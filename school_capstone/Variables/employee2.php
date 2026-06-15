c7b0fcf60892b29fec604f7031ad48a4210716ed
Victoria Young <50598538+Vyoung1998@users.noreply.github.com>
2026-06-14 23:44:25 -0400

    Week 4 is completed
    Includes finalization of Sessions folder. All photos are from Unsplash or chatgpt

---
+++ school_capstone/Variables/employee2.php
@@ -0,0 +1,82 @@
+<!--
+CSIS 410: Web Development: Variables Assignment
+By: Victoria Young
+Date Due: 5/25/2026
+
+References: 
+
+Powers, D. (2021). Php 8 solutions : Dynamic web design and development made easy. Apress L. P.
+
+Ruvalcaba, Z., Boehm, A., & Delamater, M. (2024). Murach’s HTML and CSS (J. Murach & S. McCoy, Eds.; 6th ed.) [Paperback]. Mike Murach & Associates, Inc.
+
+W3Schools.com. (2026). W3schools. Retrieved May 24, 2026, from https://www.w3schools.com/pHp/php_includes.asp
+
+Purpose of this page: This page displays the information for employee 2: Jason Hallow.
+
+Honesty Statement: I promise I wrote this cord, line by line,
+not using any help or any previous code.
+-->
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+    <head>
+        <title>Variables: Jason</title>
+        <link rel="stylesheet" href="varstyles.css">
+        <meta charset="UTF-8">
+        <meta name="description" content="PHP CSS and HTML Website CAPSTONE.">
+        <meta name="author" content="Victoria Young">
+        
+    </head>
+
+    <body>
+   <!--menu-->
+    <div class="wrapper">
+        <div class="menutitle">
+            <h1>Let it Bean</h1>
+        </div>
+        <div class="menu">
+            <?php 
+             //show menu
+            include 'menu.php'; ?>
+        </div>
+    </div>
+    <!--end menu-->
+    
+    <!--main content-->
+    <div>
+    <!--left column-->
+    <div class = "emp_left">
+        <img src="Jason.png" alt="A young man barista with blonde hair and brown eyes. He is smiling" height = 200px width= auto>
+    </div>
+    <!--right column-->
+    <div class="emp_right">
+        <?php 
+        include 'variables.php';
+            echo "
+            <h1>Hello! My name is " . $emp2_name . ".</h1>
+            <hr>
+            <h3>I am " . $emp2_age . " years old.</h3>
+            <h3>My job title is " . $emp2_jobTitle . ".</h3>
+            <h3>My favorite drink is a " . $emp2_favDrink . ".</h3>
+            <h3>My favorite scripture in the Bible is " . $emp2_favScripture . ".</h3>
+            ";
+        ?>
+    </div>
+    </div>
+
+    <!--footer-->
+    <div class = "footer">
+        <div class="footer-left">
+        <a href="../index.php"><h3>Return to Master Menu</h3></a>
+        </div>
+        <div class="footer-right">
+        <?php
+        //show last modified
+        include 'lastmodified.php';
+        ?>
+        </div>
+    </div>
+    <!--end footer-->
+    </body>
+</html>
