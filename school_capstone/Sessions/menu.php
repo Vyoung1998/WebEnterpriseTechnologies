edb80a591ff13249b0aecc66055f4c6e16045eca
Victoria Young <50598538+Vyoung1998@users.noreply.github.com>
2026-06-25 16:48:39 -0400

    Delete school_capstone directory

--- school_capstone/Sessions/menu.php
+++
@@ -1,8 +0,0 @@
-<nav>
-    <ul>
-        <li><a href="login.php">Login</a></li>
-        <li><a href="home_loggedin.php">Home</a></li>
-    </ul>
-</nav>
-
-<!--this was found from the book provided in class already sited on main pages.-->
